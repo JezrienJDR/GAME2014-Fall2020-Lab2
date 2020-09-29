355b5a7a09ca5e6ed31f3b0950afd1ab866fe7b9
JezrienJDR <JDRwrites@gmail.com>
2020-09-29 14:50:38 -0400

    Lab2 Submission
    Adaptive UI

---
+++ Assets/TestButtonBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TestButtonBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    public TMP_Text SceneLabel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTestButtonPressed()
+    {
+        Debug.Log("TestButtonPressed");
+        SceneLabel.text = "Test"; 
+    }
+}
---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameController : MonoBehaviour
+{
+    public TMP_Text SceneLabel;
+    public TMP_Text LivesLabel;
+    public TMP_Text ScoreLabel;
+
+    private bool hasSafeArea = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        hasSafeArea = !(Screen.width == Screen.safeArea.width && Screen.height == Screen.safeArea.height);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        //Debug.Log(Screen.orientation.ToString());
+
+        switch (Screen.orientation)
+        {
+            case ScreenOrientation.Portrait:
+                //SceneLabel.text = "Portrait";
+                if (hasSafeArea)
+                {
+                    LivesLabel.rectTransform.anchoredPosition = new Vector2(248.0f, -200.0f);
+                    ScoreLabel.rectTransform.anchoredPosition = new Vector2(-350.0f, -200.0f);
+                }
+                else
+                {
+                    LivesLabel.rectTransform.anchoredPosition = new Vector2(248.0f, -80.0f);
+                    ScoreLabel.rectTransform.anchoredPosition = new Vector2(-350.0f, -80.0f);
+                }
+                break;
+            case ScreenOrientation.LandscapeLeft:
+                if (hasSafeArea)
+                {
+                    LivesLabel.rectTransform.anchoredPosition = new Vector2(370.0f, -80.0f);
+                    ScoreLabel.rectTransform.anchoredPosition = new Vector2(-350.0f, -80.0f);
+                }
+                else
+                {
+                    LivesLabel.rectTransform.anchoredPosition = new Vector2(248.0f, -80.0f);
+                    ScoreLabel.rectTransform.anchoredPosition = new Vector2(-350.0f, -80.0f);
+                }
+                    break;
+            case ScreenOrientation.LandscapeRight:
+                if (hasSafeArea)
+                {
+                    LivesLabel.rectTransform.anchoredPosition = new Vector2(250.0f, -80.0f);
+                    ScoreLabel.rectTransform.anchoredPosition = new Vector2(-500.0f, -80.0f);
+                }
+                else
+                {
+                    LivesLabel.rectTransform.anchoredPosition = new Vector2(250.0f, -80.0f);
+                    ScoreLabel.rectTransform.anchoredPosition = new Vector2(-250.0f, -80.0f);
+                }
+                    break;
+            case ScreenOrientation.Unknown:
+                SceneLabel.text = "UNKNOWN";
+                break;
+        }
+    }
+}
